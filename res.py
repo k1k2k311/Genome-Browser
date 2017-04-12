8c96608e4f973c4640ee1b953f1053e548a91194
k1k2k311 <kardo41@gmail.com>
2017-04-12 12:23:06 +0100

    Update res.py

--- res.py
+++ res.py
@@ -28,9 +28,21 @@ def single_res(enzyme, sequence):
 	for match in find:
 		find_st=match.start()
 		find_end=match.end()
-		store_list_single.append((enzyme,str(find_st),str(find_end))
+		store_list_single.append((enzyme,str(find_st),str(find_end)))
 	return store_list_single
-					 
 
+store_list_custom=[]  gtagta   r'gtagta
+					 
+def custom_search_res(res_seq,sequence):
+	pattern = re.compile(res_seq)
+	find= pattern.finditer(sequence)
+	for match in find:
+		find_st=match.start()
+		find_end=match.end()
+		store_list_custom.append((res_seq,str(find_st),str(find_end)))
+	return stroe_list_custom
+	
+	
+					 
 	
 
