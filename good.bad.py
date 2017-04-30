6acf1944eef1de5bd4a642252dd6ee715d7a8853
k1k2k311 <kardo41@gmail.com>
2017-04-30 15:29:35 +0100

    Update good.bad.py

--- good.bad.py
+++ good.bad.py
@@ -22,18 +22,27 @@ extract_seq= Regexes_full.p_trans
 re_site=res.single_res(extract_seq)
 
 # expected result [(enzyme, start,end),(enzyme,start,end)...]
+ 
+# First we set bad as flase
 
+# If any restirction enzyme site is bad(between x),for loop is broken
+# and bad is returned, otherwise the site is deem to be good
 
 def good_bad():
+    bad = False
     for k in re_site:
-        if k[2]>codon_s and re_site[3]<codon_e:
-            return print("good")
+        if (int(k[1])>codon_s and int(k[2])<codon_e):
+            return ("bad")
+            bad = True
+            break
         else:
-            return print ("bad") 
+            pass
+        if not bad:
+            return ("good") 
 # test
 
 codon_s=1588
 codon_e=3588
-re_site=[('BAMHI','245','348'),('BAMHI,1023,1028')]
+re_site=[('BAMHI','150','220'),('BAMHI','1023','1028')]
 
 print(good_bad())
