58099fa82edc51a39cd580679ef33819f06b2fdb
k1k2k311 <kardo41@gmail.com>
2017-05-01 19:30:44 +0100

    Update precal.py

--- precal.py
+++ precal.py
@@ -6,20 +6,28 @@ import search
 
 import data_access
 
-
+import csv  
 data_access.get_all_coding_seqs() = extract_seq
 
 
-def chr3precal_per:
+def chr3precal_per():
     return codoncal.count_perce_codons (extract_seq)
 
 # expectred result : { 'codon' : percentage, ... } dictionary form
 
 
-def chr3precal_rev:
+def chr3precal_rev():
     return codoncal.relev_perce_codons (extract_seq)
 
 # expected result" [('codon','AA','rev_per'),(...)....]
+
+
+with open('./chromosome_codon_usage.csv', 'w') as csvfile:
+    fieldnames = ['Codon', 'percentage','relevant ratio']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'codon':chr3precal_rev()[0], 'percentage' : 'chr3precal_per[chr3precal_rev()[0]]','ratio' : 'chr3precal_rev[2]})
+        
 
 
 
